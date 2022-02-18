6addf716895053a3ed775c4e89ed2665fe91c786
Ryan Keeler <keeler.rm@gmail.com>
2022-02-18 15:41:45 -0800

    cleaned up the project by deleting several directories. unmade the decision to go django. going flask instead. taking user input in gather_financials.py with argparse rather than input variable

--- gather_financials.py
+++ gather_financials.py
@@ -1,6 +1,12 @@
 from modules.classes import company
+import argparse
 
-ticker = input('Which company to gather? (Ticker Symbol)')
+parser = argparse.ArgumentParser(description = 'Take a stock ticker symbol and gather company financials from Yahoo Finance.')
+parser.add_argument('ticker', type = str, help = 'All-caps ticker symbol of a company on Yahoo Finance. Script will gather all three financial statements for that company.')
+args = parser.parse_args()
+
+
+ticker = args.ticker
 
 co = company(ticker, method = 'scrape')
 
