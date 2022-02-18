72a24f9ac50daa9c8ca1c1347f15746f5c942f1a
Ryan Keeler <keeler.rm@gmail.com>
2022-02-18 15:43:01 -0800

    completed changes from last commit to gather_financials.py

--- gather_financials.py
+++ gather_financials.py
@@ -5,7 +5,6 @@ parser = argparse.ArgumentParser(description = 'Take a stock ticker symbol and g
 parser.add_argument('ticker', type = str, help = 'All-caps ticker symbol of a company on Yahoo Finance. Script will gather all three financial statements for that company.')
 args = parser.parse_args()
 
-
 ticker = args.ticker
 
 co = company(ticker, method = 'scrape')
